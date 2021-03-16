9e6598c71d028f07c08f44bbdb72ca7306fd8574
Edgar <cardenasedgar123@gmail.com>
2021-03-15 22:00:16 -0500

    simple flask api

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'store1',
+        'items': [
+            {
+                'name': 'item1',
+                'price': 15.85
+            }
+        ]
+    }
+]
+
+# POST - user to retrieve data
+# GET - used to send data back only (Default method is GET)
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    # pass in stores as a dictionary since it is currently a list
+    # Raw json is just a string, but it must be in the format of a dictionary
+    return jsonify({ 'stores': stores })
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_from_store(name):
+    pass
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
