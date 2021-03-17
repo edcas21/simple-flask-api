758c40c60cb890231d52469c0aa530a0a5d76504
Edgar <cardenasedgar123@gmail.com>
2021-03-16 22:24:36 -0400

    Implemented endpoint logic and remaining routes. Added an index template and a postman collection file for testing.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -14,17 +14,33 @@ stores = [
     }
 ]
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 # POST - user to retrieve data
 # GET - used to send data back only (Default method is GET)
 
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    # Iterate over stores
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+
+    return jsonify({'message': f"{name} store doesn't exist"})
 
 # GET /store
 @app.route('/store')
@@ -36,13 +52,30 @@ def get_stores():
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+
+    request_data = request.get_json()
+
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+
+    return jsonify({'message': f"store {name} was not found"})
+
+
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_items_from_store(name):
-    pass
 
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
 
+    return jsonify({'message': f"store {name} was not found"})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
